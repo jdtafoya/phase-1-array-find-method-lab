fa9da68b4704001f729b8af939dd4c960aa87550
Joshua Tafoya <tafoyajd.91@gmail.com>
2023-05-31 22:37:45 -0700

    find these amazing functions

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+function didWin(season) {
+    return season.result === "W";
+}
+
+function superbowlWin(array) {
+    let won = array.find(didWin);
+    if (won === undefined) {
+        return won;
+    } else {
+        return won.year;
+    }
+}
